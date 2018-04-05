1f70823419045c9dd5d664cd5e0e3b10b4924961
George Higbie <gehigbie@gmail.com>
2018-04-05 17:25:09 +0000

    Reconnfigure app

--- src/RouterApp.js
+++ src/RouterApp.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {} from 'react-router-dom';
 import './RouterApp.css';
+
+import Home from './components/Home/Home';
+import Posts from './components/Posts/Posts';
+import Profile from './components/Profile/Profile';
 
 class App extends Component {
   render() {
     return (
       <div className="RouterApp">
-        
+        <Home />
+        <Posts />
+        <Profile />
       </div>
     );
   }
