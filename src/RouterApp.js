508cf04df91ffe9f1b99a09e22163c844cb2a65f
George Higbie <gehigbie@gmail.com>
2018-04-05 21:35:34 +0000

    Fix folder path

--- src/RouterApp.js
+++ src/RouterApp.js
@@ -5,6 +5,7 @@ import './RouterApp.css';
 import Home from './components/Home/Home';
 import Posts from './components/Posts/Posts';
 import Profile from './components/Profile/Profile';
+import PostItem from './components/PostItem/PostItem';
 
 class App extends Component {
   render() {
@@ -23,7 +24,8 @@ class App extends Component {
             <hr/>
           </header>
           <Route path="/" exact component={Home} />
-          <Route path="/posts" component={Posts} />
+          <Route path="/posts" exact component={Posts} />
+          <Route path="/posts/:id" component={PostItem} />
           <Route path="/profile" component={Profile} />
         </div>
       </BrowserRouter>
