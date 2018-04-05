89b968d63e2afafe8952f7c11a59d0f15eb7dbc5
George Higbie <gehigbie@gmail.com>
2018-04-05 21:30:15 +0000

    Add post route

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Posts = () => {
     return(
         <div>
-            <h1>Posts</h1>
+            <Link to="/post/1">post 1</Link><br/>
+            <Link to="/post/2">post 2</Link><br/>
+            <Link to="/post/3">post 3</Link><br/>
         </div>
     );  
 };
