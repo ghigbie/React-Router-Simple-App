7c0feb258ae68e56ac8e77c9e03da037cd1fb649
George Higbie <gehigbie@gmail.com>
2018-04-06 17:08:13 +0000

    Add container

--- src/RouterApp.js
+++ src/RouterApp.js
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <div className="container">
           <header>
             <NavLink to="/"
                      activeStyle={{color:'red'}}>Home</NavLink><br/>
