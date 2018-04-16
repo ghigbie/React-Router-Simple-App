ccf7959f6e6b82cfc1af79ea71683a32ef6c9257
George Higbie <gehigbie@gmail.com>
2018-04-16 15:26:34 +0000

    Add full component

--- src/components/Lifecycles/Lifecycles.js
+++ src/components/Lifecycles/Lifecycles.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 
 class Lifecycle extends Component{
     //this component convers the lifecycle of a component
+    
+    componentWillMount(){
+        console.log('before render');
+    }
+    
+    render(){
+        return(
+            <div>
+                <h3></h3>
+            </div>
+        );
+    }
+    
+    componentDidMount(){
+        console.log('after render');
+    }
+    
 }
 
 export default Lifecycle;
