de4560d8601ae1d4a61ddce81f4929d72be5d97b
George Higbie <gehigbie@gmail.com>
2018-04-16 15:27:50 +0000

    Add state to component

--- src/components/Lifecycles/Lifecycles.js
+++ src/components/Lifecycles/Lifecycles.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
 class Lifecycle extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            title: 'Lifecycle Component'
+        };
+    }
     //this component convers the lifecycle of a component
+    
     
     componentWillMount(){
         console.log('before render');
